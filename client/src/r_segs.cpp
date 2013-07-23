AlexMax/odamex-old

commit 74b70d5df4a10507534b1d5b3bba45a8f90701f5

-Rework the two masked column blasters to eliminate two divides per column.
git-svn-id: http://odamex.net/svn/root/branches/rend32@3969 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/r_segs.cpp
@@ -195,42 +195,22 @@ static inline void R_BlastMaskedSegColumn(void (*drawfunc)())
 
 		while (!post->end())
 		{
-			if (post->length > 0)
+			// calculate unclipped screen coordinates for post
+			int topscreen = sprtopscreen + spryscale * post->topdelta;
+			int bottomscreen = topscreen + spryscale * (post->length - 1);
+
+			dc_yl = (topscreen + FRACUNIT - 1) >> FRACBITS;
+			dc_yh = (bottomscreen - 1) >> FRACBITS;
+
+			dc_yl = MAX(dc_yl, mceilingclip[dc_x] + 1);
+			dc_yh = MIN(dc_yh, mfloorclip[dc_x] - 1);
+
+			if (dc_yl <= dc_yh && dc_yl >= 0 && dc_yh < viewheight)
 			{
-				// calculate unclipped screen coordinates for post
-				int topscreen = sprtopscreen + spryscale * post->topdelta + 1;
-
-				dc_yl = (topscreen + FRACUNIT) >> FRACBITS;
-				dc_yh = (topscreen + spryscale * post->length) >> FRACBITS;
-
-				if (dc_yh >= mfloorclip[dc_x])
-					dc_yh = mfloorclip[dc_x] - 1;
-				if (dc_yl <= mceilingclip[dc_x])
-					dc_yl = mceilingclip[dc_x] + 1;
-
-				dc_texturefrac = dc_texturemid - (post->topdelta << FRACBITS)
-					+ (dc_yl*dc_iscale) - FixedMul(centeryfrac-FRACUNIT, dc_iscale);
-
-				if (dc_texturefrac < 0)
-				{
-					int cnt = (FixedDiv(-dc_texturefrac, dc_iscale) + FRACUNIT - 1) >> FRACBITS;
-					dc_yl += cnt;
-					dc_texturefrac += cnt * dc_iscale;
-				}
-
-				const fixed_t endfrac = dc_texturefrac + (dc_yh-dc_yl)*dc_iscale;
-				const fixed_t maxfrac = post->length << FRACBITS;
-				
-				if (endfrac >= maxfrac)
-				{
-					int cnt = (FixedDiv(endfrac - maxfrac - 1, dc_iscale) + FRACUNIT - 1) >> FRACBITS;
-					dc_yh -= cnt;
-				}
+				dc_texturefrac = dc_texturemid - (post->topdelta << FRACBITS) + (dc_yl - centery) * dc_iscale;
 
 				dc_source = post->data();
-
-				if (dc_yl >= 0 && dc_yh < viewheight && dc_yl <= dc_yh)
-					drawfunc();
+				drawfunc(); 
 			}
 			
 			post = post->next();
@@ -253,7 +233,7 @@ static inline void R_BlastSolidSegColumn(void (*drawfunc)())
 	dc_iscale = 0xffffffffu / unsigned(wallscalex[dc_x]);
 	dc_source = dc_post->data();
 	// TODO: dc_texturefrac should take y-scaling of textures into account
-	dc_texturefrac = dc_texturemid + FixedMul((dc_yl - centery + 1) << FRACBITS, dc_iscale);
+	dc_texturefrac = dc_texturemid + dc_iscale * (dc_yl - centery + 1);
 
 	if (dc_yl <= dc_yh)
 		drawfunc();

client/src/r_things.cpp
@@ -649,42 +649,22 @@ void R_BlastSpriteColumn(void (*drawfunc)())
 
 	while (!post->end())
 	{
-		if (post->length > 0)
+		// calculate unclipped screen coordinates for post
+		int topscreen = sprtopscreen + spryscale * post->topdelta;
+		int bottomscreen = topscreen + spryscale * (post->length - 1);
+
+		dc_yl = (topscreen + FRACUNIT - 1) >> FRACBITS;
+		dc_yh = (bottomscreen - 1) >> FRACBITS;
+
+		dc_yl = MAX(dc_yl, mceilingclip[dc_x] + 1);
+		dc_yh = MIN(dc_yh, mfloorclip[dc_x] - 1);
+
+		if (dc_yl <= dc_yh && dc_yl >= 0 && dc_yh < viewheight)
 		{
-			// calculate unclipped screen coordinates for post
-			int topscreen = sprtopscreen + spryscale * post->topdelta + 1;
+			dc_texturefrac = dc_texturemid - (post->topdelta << FRACBITS) + (dc_yl - centery) * dc_iscale;
 
-			dc_yl = (topscreen + FRACUNIT) >> FRACBITS;
-			dc_yh = (topscreen + spryscale * post->length) >> FRACBITS;
-
-			dc_yl = MAX(dc_yl, mceilingclip[dc_x] + 1);
-			dc_yh = MIN(dc_yh, mfloorclip[dc_x] - 1);
-
-			if (dc_yl >= 0 && dc_yh < viewheight && dc_yl <= dc_yh)
-			{
-				dc_texturefrac = dc_texturemid - (post->topdelta << FRACBITS)
-					+ (dc_yl*dc_iscale) - FixedMul(centeryfrac-FRACUNIT, dc_iscale);
-
-				if (dc_texturefrac < 0)
-				{
-					int cnt = (FixedDiv(-dc_texturefrac, dc_iscale) + FRACUNIT - 1) >> FRACBITS;
-					dc_yl += cnt;
-					dc_texturefrac += cnt * dc_iscale;
-				}
-
-				const fixed_t endfrac = dc_texturefrac + (dc_yh-dc_yl)*dc_iscale;
-				const fixed_t maxfrac = post->length << FRACBITS;
-				
-				if (endfrac >= maxfrac)
-				{
-					int cnt = (FixedDiv(endfrac - maxfrac - 1, dc_iscale) + FRACUNIT - 1) >> FRACBITS;
-					dc_yh -= cnt;
-				}
-
-				dc_source = post->data();
-
-				drawfunc();
-			}
+			dc_source = post->data();
+			drawfunc(); 
 		}
 		
 		post = post->next();

commit a9b263e6f32e245a06901252208a82edfa22f1d7

-Fix a masked column scaling bug introduced in r3969.
git-svn-id: http://odamex.net/svn/root/branches/rend32@4010 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/r_segs.cpp
@@ -197,7 +197,7 @@ static inline void R_BlastMaskedSegColumn(void (*drawfunc)())
 		{
 			// calculate unclipped screen coordinates for post
 			int topscreen = sprtopscreen + spryscale * post->topdelta;
-			int bottomscreen = topscreen + spryscale * (post->length - 1);
+			int bottomscreen = topscreen + spryscale * post->length;
 
 			dcol.yl = (topscreen + FRACUNIT - 1) >> FRACBITS;
 			dcol.yh = (bottomscreen - 1) >> FRACBITS;
@@ -205,7 +205,10 @@ static inline void R_BlastMaskedSegColumn(void (*drawfunc)())
 			dcol.yl = MAX(dcol.yl, mceilingclip[dcol.x] + 1);
 			dcol.yh = MIN(dcol.yh, mfloorclip[dcol.x] - 1);
 
-			if (dcol.yl <= dcol.yh && dcol.yl >= 0 && dcol.yh < viewheight)
+			dcol.yl = MAX(dcol.yl, 0);
+			dcol.yh = MIN(dcol.yh, viewheight - 1);
+
+			if (dcol.yl <= dcol.yh)
 			{
 				dcol.texturefrac = dcol.texturemid - (post->topdelta << FRACBITS) + (dcol.yl - centery) * dcol.iscale;
 

client/src/r_things.cpp
@@ -651,7 +651,7 @@ void R_BlastSpriteColumn(void (*drawfunc)())
 	{
 		// calculate unclipped screen coordinates for post
 		int topscreen = sprtopscreen + spryscale * post->topdelta;
-		int bottomscreen = topscreen + spryscale * (post->length - 1);
+		int bottomscreen = topscreen + spryscale * post->length;
 
 		dcol.yl = (topscreen + FRACUNIT - 1) >> FRACBITS;
 		dcol.yh = (bottomscreen - 1) >> FRACBITS;
@@ -659,7 +659,10 @@ void R_BlastSpriteColumn(void (*drawfunc)())
 		dcol.yl = MAX(dcol.yl, mceilingclip[dcol.x] + 1);
 		dcol.yh = MIN(dcol.yh, mfloorclip[dcol.x] - 1);
 
-		if (dcol.yl <= dcol.yh && dcol.yl >= 0 && dcol.yh < viewheight)
+		dcol.yl = MAX(dcol.yl, 0);
+		dcol.yh = MIN(dcol.yh, viewheight - 1);
+
+		if (dcol.yl <= dcol.yh)
 		{
 			dcol.texturefrac = dcol.texturemid - (post->topdelta << FRACBITS) + (dcol.yl - centery) * dcol.iscale;
 