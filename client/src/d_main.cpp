AlexMax/odamex-old

commit 11849d5a03c3f55037ec58a929e01390cbdebb02

-Refactor D_DoomWadReboot and split portions into client and server versions of D_Init and D_Shutdown. -Fixes several TextureManager crashes that occur when changing wads.
git-svn-id: http://odamex.net/svn/root/branches/texmanager@4320 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/d_main.cpp
@@ -641,41 +641,92 @@ bool HashOk(std::string &required, std::string &available)
 #endif
 }*/
 
-//
-// D_NewWadInit
-//
-// Client code that should be reset every time a new set of WADs are loaded
-//
-void D_NewWadInit()
-{
-	AM_Stop();
 
-	HU_Init ();
+//
+// D_Init
+//
+// Called to initialize subsystems when loading a new set of WAD resource
+// files.
+//
+void D_Init()
+{
+	M_ClearRandom();
+
+	// start the Zone memory manager
+	Z_Init();
+
+	R_InitTextureManager();
+
+	SetLanguageIDs();
+
+	// [RH] Initialize localizable strings.
+	GStrings.LoadStrings(W_GetNumForName("LANGUAGE"), STRING_TABLE_SIZE, false);
+	GStrings.Compact();
+
+	// fonts
+	V_LoadHudFont();
+	V_LoadMenuFont();
+	V_LoadConsoleFont();
+
+	// init the renderer
+	R_Init();
+
+	HU_Init();
 
 	if (!(InitPalettes("PLAYPAL")))
 		I_Error("Could not reinitialize palette");
 	V_InitPalette();
 
-	G_SetLevelStrings ();
-	G_ParseMapInfo ();
-	G_ParseMusInfo ();
+	G_SetLevelStrings();
+	G_ParseMapInfo();
+	G_ParseMusInfo();
 	S_ParseSndInfo();
 
+	// init the menu subsystem
 	M_Init();
 
-	texturemanager.shutdown();
-	texturemanager.startup();
-
-	R_Init();
-	V_LoadConsoleFont();
-	V_LoadHudFont();
-	V_LoadMenuFont();
-	P_InitEffects();	// [ML] Do this here so we don't have to put particle crap in server
+	P_InitEffects();
 	P_Init();
 
-	S_Init (snd_sfxvolume, snd_musicvolume);
+	// init sound and music
+	S_Init(snd_sfxvolume, snd_musicvolume);
+
+	// init the status bar
 	ST_Init();
 }
+
+//
+// D_Shutdown
+//
+// Called to shutdown subsystems when unloading a set of WAD resource files.
+// Should be called prior to D_Init when loading a new set of WADs.
+//
+void D_Shutdown()
+{
+	// stop sound effects and music
+	S_Stop();
+	
+	// shutdown automap
+	AM_Stop();
+
+	DThinker::DestroyAllThinkers();
+
+	// close all open WAD files
+	W_Close();
+
+	// fonts
+	V_UnloadConsoleFont();
+	V_UnloadMenuFont();
+	V_UnloadHudFont();
+
+	GStrings.ResetStrings();
+
+	R_ShutdownTextureManager();
+
+	// reset the Zone memory manager
+	Z_Init();
+}
+
 
 void CL_NetDemoRecord(const std::string &filename);
 void CL_NetDemoPlay(const std::string &filename);
@@ -850,8 +901,8 @@ void D_DoomMain (void)
 	Printf (PRINT_HIGH, "M_Init: Init miscellaneous info.\n");
 	M_Init ();
 
-	Printf (PRINT_HIGH, "TextureManager startup\n");
-	texturemanager.startup();
+	Printf (PRINT_HIGH, "R_InitTextureManager: Init image resource management.\n");
+	R_InitTextureManager();
 
 	Printf (PRINT_HIGH, "R_Init: Init DOOM refresh daemon.\n");
 	R_Init ();

common/d_main.cpp
@@ -1000,13 +1000,6 @@ bool D_DoomWadReboot(
 	if (gamestate == GS_LEVEL)
 		G_ExitLevel(0, 0);
 
-	S_Stop();
-
-	DThinker::DestroyAllThinkers();
-
-	// Close all open WAD files
-	W_Close();
-
 	// [ML] 9/11/10: Reset custom wad level information from MAPINFO et al.
 	for (i = 0; i < wadlevelinfos.size(); i++)
 	{
@@ -1020,8 +1013,7 @@ bool D_DoomWadReboot(
 	wadlevelinfos.clear();
 	wadclusterinfos.clear();
 
-	// Restart the memory manager
-	Z_Init();
+	D_Shutdown();
 
 	SetLanguageIDs ();
 
@@ -1093,7 +1085,7 @@ bool D_DoomWadReboot(
 
 	D_DoDefDehackedPatch(newpatchfiles);
 
-	D_NewWadInit();
+	D_Init();
 
 	// preserve state
 	lastWadRebootSuccess = missingfiles.empty();

common/r_texture.cpp
@@ -37,9 +37,17 @@
 
 #include "r_texture.h"
 
-
 TextureManager texturemanager;
 
+void R_InitTextureManager()
+{
+	texturemanager.startup();
+}
+
+void R_ShutdownTextureManager()
+{
+	texturemanager.shutdown();
+}
 
 //
 // R_DrawPatchIntoTexture
@@ -226,6 +234,19 @@ TextureManager::~TextureManager()
 //
 void TextureManager::clear()
 {
+	// free normal textures
+	for (HandleMap::iterator it = mHandleMap.begin(); it != mHandleMap.end(); ++it)
+		if (it->second)
+			Z_Free((void*)it->second);
+
+	mHandleMap.clear();
+
+	// free all custom texture handles
+	mFreeCustomHandlesHead = 0;
+	mFreeCustomHandlesTail = TextureManager::MAX_CUSTOM_HANDLES - 1;
+	for (unsigned int i = 0; i < TextureManager::MAX_CUSTOM_HANDLES; i++)
+		mFreeCustomHandles[i] = CUSTOM_HANDLE_MASK | i;
+
 	delete [] mPNameLookup;
 	mPNameLookup = NULL;
 
@@ -239,17 +260,12 @@ void TextureManager::clear()
 
 	mAnimDefs.clear();
 
+	// free warping original texture (not stored in mHandleMap)
+	for (size_t i = 0; i < mWarpDefs.size(); i++)
+		if (mWarpDefs[i].original_texture)
+			Z_Free((void*)mWarpDefs[i].original_texture);
+
 	mWarpDefs.clear();
-
-	// [SL] the zone memory manager takes care of freeing all allocated
-	// memory for Textures.
-
-	mHandleMap.clear();
-
-	mFreeCustomHandlesHead = 0;
-	mFreeCustomHandlesTail = TextureManager::MAX_CUSTOM_HANDLES;
-	for (unsigned int i = 0; i < TextureManager::MAX_CUSTOM_HANDLES; i++)
-		mFreeCustomHandles[i] = CUSTOM_HANDLE_MASK | i;
 }
 
 
@@ -299,11 +315,19 @@ void TextureManager::shutdown()
 //
 void TextureManager::freeTexture(texhandle_t texhandle)
 {
+	if (texhandle == TextureManager::NOT_FOUND_TEXTURE_HANDLE ||
+		texhandle == TextureManager::NO_TEXTURE_HANDLE)
+		return;
+
 	Texture* texture = mHandleMap[texhandle];
-	Z_Free((void*)texture);
+	if (texture)
+	{
+		Z_Free((void*)texture);
+		if (texhandle & CUSTOM_HANDLE_MASK)
+			freeCustomHandle(texhandle);
+	}
+
 	mHandleMap.erase(texhandle);
-	if (texhandle & CUSTOM_HANDLE_MASK)
-		freeCustomHandle(texhandle);
 }
 
 //
@@ -681,6 +705,7 @@ void TextureManager::generateNotFoundTexture()
 
 	const texhandle_t handle = NOT_FOUND_TEXTURE_HANDLE;
 	Texture* texture = createTexture(handle, width, height);
+	Z_ChangeTag(texture, PU_STATIC);
 
 	if (clientside)
 	{
@@ -820,6 +845,9 @@ texhandle_t TextureManager::createCustomHandle()
 	return mFreeCustomHandles[mFreeCustomHandlesHead++ % MAX_CUSTOM_HANDLES];
 }
 
+//
+// TextureManager::freeCustomHandle
+//
 void TextureManager::freeCustomHandle(texhandle_t texhandle)
 {
 	mFreeCustomHandles[mFreeCustomHandlesTail++ % MAX_CUSTOM_HANDLES] = texhandle;

common/r_texture.h
@@ -37,10 +37,10 @@ typedef unsigned int texhandle_t;
 class Texture;
 class TextureManager;
 
+void R_InitTextureManager();
+void R_ShutdownTextureManager();
 
 void R_CopySubimage(Texture* dest_texture, const Texture* source_texture, int x1, int y1, int x2, int y2);
-
-
 
 
 // ============================================================================
@@ -172,7 +172,6 @@ public:
 	void startup();
 	void shutdown();
 
-	void clear();
 	void precache();
 
 	void updateAnimatedTextures();
@@ -182,7 +181,6 @@ public:
 	const Texture* getTexture(texhandle_t handle);
 
 	Texture* createTexture(texhandle_t handle, int width, int height);
-
 	void freeTexture(texhandle_t handle);
 
 	texhandle_t createCustomHandle();
@@ -203,6 +201,7 @@ private:
 	static const unsigned int MAX_TEXTURE_HEIGHT		= 2048;
 
 	// initialization routines
+	void clear();
 	void generateNotFoundTexture();
 	void readPNamesDirectory();
 	void addTextureDirectory(const char* lumpname); 

server/src/d_main.cpp
@@ -301,23 +301,70 @@ void D_StartTitle (void)
 #endif
 }*/
 
-void D_NewWadInit()
+//
+// D_Init
+//
+// Called to initialize subsystems when loading a new set of WAD resource
+// files.
+//
+void D_Init()
 {
-	if (DefaultsLoaded)	{		// [ML] This is being called while loading defaults,
-		G_SetLevelStrings ();
-		G_ParseMapInfo ();
-		G_ParseMusInfo ();
-		S_ParseSndInfo();
+	M_ClearRandom();
 
-		texturemanager.shutdown();
-		texturemanager.startup();
+	// start the Zone memory manager
+	Z_Init();
+
+	SetLanguageIDs();
+
+	// [RH] Initialize localizable strings.
+	GStrings.LoadStrings(W_GetNumForName("LANGUAGE"), STRING_TABLE_SIZE, false);
+	GStrings.Compact();
+
+	R_InitTextureManager();
+
+	if (DefaultsLoaded)
+	{
+		// [ML] This is being called while loading defaults,
+		G_SetLevelStrings();
+		G_ParseMapInfo();
+		G_ParseMusInfo();
+		S_ParseSndInfo();
 
 		R_Init();
 		P_Init();
-	} else {					// let DoomMain know it doesn't have to do everything
+	}
+	else
+	{
+		// let DoomMain know it doesn't have to do everything
 		RebootInit = true;
 	}
 }
+
+
+//
+// D_Shutdown
+//
+// Called to shutdown subsystems when unloading a set of WAD resource files.
+// Should be called prior to D_Init when loading a new set of WADs.
+//
+void D_Shutdown()
+{
+	// stop sound effects and music
+	S_Stop();
+	
+	DThinker::DestroyAllThinkers();
+
+	// close all open WAD files
+	W_Close();
+
+	GStrings.ResetStrings();
+
+	R_ShutdownTextureManager();
+
+	// reset the Zone memory manager
+	Z_Init();
+}
+
 
 //
 // D_DoomMain
@@ -440,8 +487,8 @@ void D_DoomMain (void)
 	if (modifiedgame && (gameinfo.flags & GI_SHAREWARE))
 		I_Error ("You cannot -file with the shareware version. Register!");
 
-	Printf (PRINT_HIGH, "TextureManager startup.\n");
-	texturemanager.startup();
+	Printf (PRINT_HIGH, "R_InitTextureManager: Init image resource management.\n");
+	R_InitTextureManager();
 
 	Printf (PRINT_HIGH, "R_Init: Init DOOM refresh daemon.\n");
 	R_Init ();

client/src/g_level.cpp
@@ -490,24 +490,7 @@ void G_DoLoadLevel (int position)
 	gamestate = GS_LEVEL;
 
 
-	// Set the sky map.
-	// First thing, we have a dummy sky texture name,
-	//	a flat. The data is in the WAD only because
-	//	we look for an actual index, instead of simply
-	//	setting one.
-	sky1flathandle = texturemanager.getHandle(SKYFLATNAME, Texture::TEX_FLAT);
-	sky2flathandle = texturemanager.createCustomHandle();
-
-	// DOOM determines the sky texture to be used
-	// depending on the current episode, and the game version.
-	// [RH] Fetch sky parameters from level_locals_t.
-	// [ML] 5/11/06 - remove sky2 remenants
-	// [SL] 2012-03-19 - Add sky2 back
-	sky1texhandle = texturemanager.getHandle(level.skypic, Texture::TEX_WALLTEXTURE);
-	if (strlen(level.skypic2))
-		sky2texhandle = texturemanager.getHandle(level.skypic2, Texture::TEX_WALLTEXTURE);
-	else
-		sky2texhandle = 0;
+	G_InitSkyTextures(level.skypic, level.skypic2);
 
 	// [RH] Set up details about sky rendering
 	R_InitSkyMap ();

client/src/r_sky.cpp
@@ -70,8 +70,6 @@ CVAR_FUNC_IMPL(r_stretchsky)
 	R_InitSkyMap ();
 }
 
-char SKYFLATNAME[8] = "F_SKY1";
-
 extern "C" int detailxshift, detailyshift;
 extern fixed_t freelookviewheight;
 
@@ -134,7 +132,7 @@ private:
 // [ML] 5/11/06 - Remove sky2 stuffs
 // [ML] 3/16/10 - Bring it back!
 
-void R_InitSkyMap ()
+void R_InitSkyMap()
 {
 	// [SL] 2011-11-30 - Don't run if we don't know what sky texture to use
 	if (gamestate != GS_LEVEL)
@@ -180,7 +178,7 @@ void R_InitSkyMap ()
 	sky2shift = 22+skystretch-16;	
 	if (sky1texture->getWidth() >= 128)
 		sky1shift -= skystretch;
-	if (sky2texture->getWidth() >= 128)
+	if (sky2texture && sky2texture->getWidth() >= 128)
 		sky2shift -= skystretch;
 }
 

common/c_level.cpp
@@ -283,7 +283,6 @@ void G_ParseMapInfo (void)
 				{	// MAPNAME is a number, assume a Hexen wad
 					int map = atoi (sc_String);
 					sprintf (sc_String, "MAP%02d", map);
-					SKYFLATNAME[5] = 0;
 					HexenHack = true;
 					// Hexen levels are automatically nointermission
 					// and even lighting and no auto sound sequences
@@ -488,6 +487,41 @@ void P_RemoveDefereds (void)
 void G_ParseMusInfo(void)
 {
 	// Nothing yet...
+}
+
+//
+// G_InitSkyTextures
+//
+// Initializes sky1flathandle and sky2flathandle and sets
+// sky1texhandle and sky2texhandle to the appropriate textures based
+// on the given names.
+//
+// Set the sky map.
+// First thing, we have a dummy sky texture name,
+//	a flat. The data is in the WAD only because
+//	we look for an actual index, instead of simply
+//	setting one.
+//
+// DOOM determines the sky texture to be used
+// depending on the current episode, and the game version.
+// [RH] Fetch sky parameters from level_locals_t.
+// [ML] 5/11/06 - remove sky2 remenants
+// [SL] 2012-03-19 - Add sky2 back
+//
+void G_InitSkyTextures(const char* texture_name1, const char* texture_name2)
+{
+	char SKYFLATNAME[8] = "F_SKY1";
+	if (HexenHack)
+		SKYFLATNAME[5] = 0;
+
+	sky1flathandle = texturemanager.getHandle(SKYFLATNAME, Texture::TEX_FLAT);
+	sky2flathandle = texturemanager.createCustomHandle();
+
+	sky1texhandle = texturemanager.getHandle(texture_name1, Texture::TEX_WALLTEXTURE);
+	if (strlen(texture_name2) > 0)
+		sky2texhandle = texturemanager.getHandle(texture_name2, Texture::TEX_WALLTEXTURE);
+	else
+		sky2texhandle = TextureManager::NO_TEXTURE_HANDLE;
 }
 
 //

commit 01b0357c48c57938954295829826320daba5cfd0

-Fix a crash introduced in r4320. Don't call GStrings.ResetStrings() prior to calling Z_Init as it frees memory previously allocated by the zone memory manager. -Minor code clean up.
git-svn-id: http://odamex.net/svn/root/branches/texmanager@4321 b0bcef46-2027-0410-bd56-ab726e9a1d04

client/src/d_main.cpp
@@ -720,6 +720,7 @@ void D_Shutdown()
 	V_UnloadHudFont();
 
 	GStrings.ResetStrings();
+	GStrings.Compact();
 
 	R_ShutdownTextureManager();
 

common/d_main.cpp
@@ -1079,10 +1079,6 @@ bool D_DoomWadReboot(
     UndoDehPatch();
     patchfiles.clear();
 
-	// [RH] Initialize localizable strings.
-	GStrings.ResetStrings ();
-	GStrings.Compact ();
-
 	D_DoDefDehackedPatch(newpatchfiles);
 
 	D_Init();

common/r_texture.cpp
@@ -150,6 +150,58 @@ void R_CopySubimage(Texture* dest_texture, const Texture* source_texture, int x1
 	dest_texture->setOffsetY(yoffs);
 }
 
+
+//
+// R_WarpTexture
+//
+// Alters the image in source_texture with a warping effect and saves the
+// result in dest_texture.
+//
+static void R_WarpTexture(Texture* dest_texture, const Texture* source_texture)
+{
+	const byte* source_buffer = source_texture->getData();
+	byte* warped_buffer = dest_texture->getData();
+
+	int widthbits = source_texture->getWidthBits();
+	int width = (1 << widthbits);
+	int widthmask = width - 1; 
+	int heightbits = source_texture->getHeightBits();
+	int height = (1 << heightbits);
+	int heightmask = height - 1;
+
+	byte temp_buffer[Texture::MAX_TEXTURE_HEIGHT];
+
+	int step = level.time * 32;
+	for (int y = height - 1; y >= 0; y--)
+	{
+		const byte* source = source_buffer + y;
+		byte* dest = warped_buffer + y;
+
+		int xf = (finesine[(step + y * 128) & FINEMASK] >> 13) & widthmask;
+		for (int x = 0; x < width; x++)
+		{
+			*dest = source[xf << heightbits];
+			dest += height;
+			xf = (xf + 1) & widthmask;
+		}
+	}
+
+	step = level.time * 23;
+	for (int x = width - 1; x >= 0; x--)
+	{
+		const byte *source = warped_buffer + (x << heightbits);
+		byte *dest = temp_buffer;
+
+		int yf = (finesine[(step + 128 * (x + 17)) & FINEMASK] >> 13) & heightmask;
+		for (int y = 0; y < height; y++)
+		{
+			*dest++ = source[yf];
+			yf = (yf + 1) & heightmask;
+		}
+		memcpy(warped_buffer + (x << heightbits), temp_buffer, height);
+	}
+}
+
 // ============================================================================
 //
 // Texture
@@ -244,7 +296,7 @@ void TextureManager::clear()
 	// free all custom texture handles
 	mFreeCustomHandlesHead = 0;
 	mFreeCustomHandlesTail = TextureManager::MAX_CUSTOM_HANDLES - 1;
-	for (unsigned int i = 0; i < TextureManager::MAX_CUSTOM_HANDLES; i++)
+	for (unsigned int i = mFreeCustomHandlesHead; i <= mFreeCustomHandlesTail; i++)
 		mFreeCustomHandles[i] = CUSTOM_HANDLE_MASK | i;
 
 	delete [] mPNameLookup;
@@ -319,15 +371,18 @@ void TextureManager::freeTexture(texhandle_t texhandle)
 		texhandle == TextureManager::NO_TEXTURE_HANDLE)
 		return;
 
-	Texture* texture = mHandleMap[texhandle];
-	if (texture)
+	HandleMap::iterator it = mHandleMap.find(texhandle);
+	if (it != mHandleMap.end())
 	{
-		Z_Free((void*)texture);
-		if (texhandle & CUSTOM_HANDLE_MASK)
-			freeCustomHandle(texhandle);
-	}
+		if (it->second)
+		{
+			Z_Free((void*)it->second);
+			if (texhandle & CUSTOM_HANDLE_MASK)
+				freeCustomHandle(texhandle);
+		}
 
-	mHandleMap.erase(texhandle);
+		mHandleMap.erase(it);
+	}
 }
 
 //
@@ -648,48 +703,7 @@ void TextureManager::updateAnimatedTextures()
 	{
 		const Texture* original_texture = mWarpDefs[i].original_texture;
 		Texture* warped_texture = mWarpDefs[i].warped_texture;
-
-		const byte* original_buffer = original_texture->getData();
-		byte* warped_buffer = warped_texture->getData();
-
-		int widthbits = original_texture->getWidthBits();
-		int width = (1 << widthbits);
-		int widthmask = width - 1; 
-		int heightbits = original_texture->getHeightBits();
-		int height = (1 << heightbits);
-		int heightmask = height - 1;
-
-		byte temp_buffer[TextureManager::MAX_TEXTURE_HEIGHT];
-
-		int step = level.time * 32;
-		for (int y = height - 1; y >= 0; y--)
-		{
-			const byte* source = original_buffer + y;
-			byte* dest = warped_buffer + y;
-
-			int xf = (finesine[(step + y * 128) & FINEMASK] >> 13) & widthmask;
-			for (int x = 0; x < width; x++)
-			{
-				*dest = source[xf << heightbits];
-				dest += height;
-				xf = (xf + 1) & widthmask;
-			}
-		}
-
-		step = level.time * 23;
-		for (int x = width - 1; x >= 0; x--)
-		{
-			const byte *source = warped_buffer + (x << heightbits);
-			byte *dest = temp_buffer;
-
-			int yf = (finesine[(step + 128 * (x + 17)) & FINEMASK] >> 13) & heightmask;
-			for (int y = 0; y < height; y++)
-			{
-				*dest++ = source[yf];
-				yf = (yf + 1) & heightmask;
-			}
-			memcpy(warped_buffer + (x << heightbits), temp_buffer, height);
-		}
+		R_WarpTexture(warped_texture, original_texture);
 	}
 }
 
@@ -740,21 +754,21 @@ void TextureManager::addTextureDirectory(const char* lumpname)
 	// into the rectangular texture space using origin
 	// and possibly other attributes.
 	//
-	typedef struct
+	struct mappatch_t
 	{
 		short	originx;
 		short	originy;
 		short	patch;
 		short	stepdir;
 		short	colormap;
-	} mappatch_t;
+	};
 
 	//
 	// Texture definition.
 	// A DOOM wall texture is a list of patches
 	// which are to be combined in a predefined order.
 	//
-	typedef struct
+	struct maptexture_t
 	{
 		char		name[8];
 		WORD		masked;				// [RH] Unused
@@ -765,7 +779,7 @@ void TextureManager::addTextureDirectory(const char* lumpname)
 		byte		columndirectory[4];	// OBSOLETE
 		short		patchcount;
 		mappatch_t	patches[1];
-	} maptexture_t;
+	};
 
 	int lumpnum = W_CheckNumForName(lumpname);
 	if (lumpnum == -1)
@@ -860,8 +874,8 @@ void TextureManager::freeCustomHandle(texhandle_t texhandle)
 //
 Texture* TextureManager::createTexture(texhandle_t handle, int width, int height)
 {
-	width = std::min<int>(width, TextureManager::MAX_TEXTURE_WIDTH);
-	height = std::min<int>(height, TextureManager::MAX_TEXTURE_HEIGHT);
+	width = std::min<int>(width, Texture::MAX_TEXTURE_WIDTH);
+	height = std::min<int>(height, Texture::MAX_TEXTURE_HEIGHT);
 
 	Texture** owner = &mHandleMap[handle];
 	// server shouldn't allocate memory for texture data, only the header	

common/r_texture.h
@@ -70,6 +70,9 @@ public:
 		TEX_WALLTEXTURE
 	} TextureSourceType;
 
+	static const unsigned int MAX_TEXTURE_WIDTH			= 2048;
+	static const unsigned int MAX_TEXTURE_HEIGHT		= 2048;
+
 	byte* getData() const
 	{	return mData;	}
 
@@ -197,9 +200,6 @@ private:
 	static const unsigned int WALLTEXTURE_HANDLE_MASK	= 0x00080000ul;
 	static const unsigned int CUSTOM_HANDLE_MASK		= 0x000A0000ul;
 
-	static const unsigned int MAX_TEXTURE_WIDTH			= 2048;
-	static const unsigned int MAX_TEXTURE_HEIGHT		= 2048;
-
 	// initialization routines
 	void clear();
 	void generateNotFoundTexture();
@@ -237,14 +237,14 @@ private:
 	unsigned int				mLastFlatLumpNum;
 
 	// definitions for texture composition
-	typedef struct
+	struct texdefpatch_t
 	{
 		int 		originx;
 		int 		originy;
 		int 		patch;
-	} texdefpatch_t;
+	};
 
-	typedef struct
+	struct texdef_t
 	{
 		short			width;
 		short			height;
@@ -253,7 +253,7 @@ private:
 
 		short			patchcount;
 		texdefpatch_t	patches[1];
-	} texdef_t;
+	};
 
 	int*						mPNameLookup;
 	unsigned int				mTextureDefinitionCount;
@@ -270,7 +270,7 @@ private:
 	texhandle_t					mFreeCustomHandles[MAX_CUSTOM_HANDLES];
 
 	// animated textures
-	typedef struct
+	struct anim_t
 	{
 		static const unsigned int MAX_ANIM_FRAMES = 32;
 		texhandle_t		basepic;
@@ -280,16 +280,16 @@ private:
 		byte 			speedmin[MAX_ANIM_FRAMES];
 		byte			speedmax[MAX_ANIM_FRAMES];
 		texhandle_t		framepic[MAX_ANIM_FRAMES];
-	} anim_t;
+	};
 
 	std::vector<anim_t>			mAnimDefs;
 
 	// warped textures
-	typedef struct
+	struct warp_t
 	{
 		const Texture*	original_texture;
 		Texture*		warped_texture;
-	} warp_t;
+	};
 
 	std::vector<warp_t>			mWarpDefs;
 };

server/src/d_main.cpp
@@ -358,6 +358,7 @@ void D_Shutdown()
 	W_Close();
 
 	GStrings.ResetStrings();
+	GStrings.Compact();
 
 	R_ShutdownTextureManager();
 