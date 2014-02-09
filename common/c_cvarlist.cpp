AlexMax/odamex-old

commit 4b6147d8dfc500d503fe2981fa630ba147fa8ad3

-Add ranges for the remaining common cvars. -Change the flag CVAR_ARCHIVE to be an alias for CVAR_CLIENTARCHIVE | CVAR_SERVERARCHIVE since that is its implied use.
git-svn-id: http://odamex.net/svn/root/trunk@4521 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/c_cvarlist.cpp
@@ -118,171 +118,160 @@ CVAR_RANGE(			sv_skill,"3", "Sets the skill level, values are:\n" \
 					CVARTYPE_BYTE, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH | CVAR_NOENABLEDISABLE, 0.0f, 5.0f)
 
 
-CVAR_FUNC_DECL (sv_timelimit,    "0", "Sets the time limit for the game to end, must be greater than 1",
-      CVARTYPE_WORD, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
+CVAR_RANGE_FUNC_DECL(sv_timelimit, "0", "Sets the time limit for the game to end (in minutes)",
+					CVARTYPE_WORD, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE, 0.0f, 32768.0f)
 
-// Intermission ends on intermission time limit being reached
-CVAR_FUNC_DECL (sv_intermissionlimit, "10", "Sets the time limit for the intermission to end, 0 disables (defaults to 10 seconds)",
-      CVARTYPE_WORD, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
+CVAR_RANGE_FUNC_DECL(sv_intermissionlimit, "10", "Sets the time limit for the intermission to end (in seconds)",
+					CVARTYPE_WORD, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE, 0.0f, 32768.0f)
 
-// Weapons stay
-CVAR (sv_weaponstay,    "1", "Weapons stay after pickup",
-      CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
-// Keep keys on death
-CVAR(sv_keepkeys, "0", "Keep keys on death",
-     CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
+CVAR(				sv_weaponstay,    "1", "Weapons stay after pickup",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
-// [SL] 2011-05-11 - Allow reconciliation for players on lagged connections
-CVAR (sv_unlag,            "1", "Allow reconciliation for players on lagged connections",
-      CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
-CVAR (sv_maxunlagtime,	"1.0", "Cap the maxiumum time allowed for player reconciliation",
-      CVARTYPE_FLOAT, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
-// [ML] allow weapon & view bob changing
-CVAR (sv_allowmovebob, "0", "Allow weapon & view bob changing",
-      CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				sv_keepkeys, "0", "Keep keys on death",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
-// [ML] Allow clients to adjust amount of red pain screen intensity
-CVAR (sv_allowredscreen, "0","Allow clients to adjust amount of red pain screen intensity",
-     CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE)
+CVAR(				sv_unlag, "1", "Allow reconciliation for players on lagged connections",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
-// [SL] Allow PWO
-CVAR (sv_allowpwo, "0", "Allow clients to set their preferences for automatic weapon swithching",
-   CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE)
+CVAR_RANGE(			sv_maxunlagtime, "1.0", "Cap the maxiumum time allowed for player reconciliation (in seconds)",
+					CVARTYPE_FLOAT, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE, 0.0f, 1.0f)
 
-// [AM] Allow true widescreen usage
-CVAR_FUNC_DECL (sv_allowwidescreen, "1", "Allow clients to use true widescreen",
-      CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE | CVAR_LATCH)
+CVAR(				sv_allowmovebob, "0", "Allow weapon & view bob changing",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
 
-// [AM] Allow players to see the spawns on the map
-CVAR (sv_allowshowspawns, "1", "Allow clients to see spawn points as particle fountains",
-      CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE | CVAR_LATCH)
+CVAR(				sv_allowredscreen, "0","Allow clients to adjust amount of red pain screen intensity",
+					CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE)
+
+CVAR(				sv_allowpwo, "0", "Allow clients to set their preferences for automatic weapon swithching",
+					CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE)
+
+CVAR_FUNC_DECL(		sv_allowwidescreen, "1", "Allow clients to use true widescreen",
+					CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE | CVAR_LATCH)
+
+CVAR(				sv_allowshowspawns, "1", "Allow clients to see spawn points as particle fountains",
+					CVARTYPE_BOOL, CVAR_SERVERINFO | CVAR_SERVERARCHIVE | CVAR_LATCH)
+
+CVAR(				sv_forcerespawn, "0", "Force a player to respawn",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+
+CVAR_RANGE(			sv_forcerespawntime, "30", "Force a player to respawn after a set amount of time (in seconds)",
+					CVARTYPE_WORD, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE, 0.0f, 32768.0f)
+
+CVAR_RANGE(			sv_spawndelaytime, "0.0", "Force a player to wait a period (in seconds) before respawning",
+					CVARTYPE_FLOAT, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE, 0.0f, 32768.0f)
+
+CVAR(				sv_unblockplayers, "0", "Allows players to walk through other players",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_LATCH | CVAR_SERVERINFO)
+
+CVAR(				sv_hostname, "Untitled Odamex Server", "Server name to appear on masters, clients and launchers",
+					CVARTYPE_STRING, CVAR_SERVERARCHIVE | CVAR_NOENABLEDISABLE | CVAR_SERVERINFO)
+
+
 
 // Compatibility options for vanilla
 // ---------------------------------
 
-// Enable/disable infinitely tall actors
-CVAR (co_realactorheight, "0", "Enable/Disable infinitely tall actors",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
+CVAR(				co_realactorheight, "0", "Enable/Disable infinitely tall actors",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
-// [Spleen] When enabled, monsters can get pushed or thrusted off of ledges, like in boom
-CVAR (co_allowdropoff, "0", "When enabled, monsters can get pushed or thrusted off of ledges, like in boom",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
+CVAR(				co_allowdropoff, "0", "Allow monsters can get pushed or thrusted off of ledges",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
-// [ML] When enabled, activate correct impact of projectiles and bullets on surfaces (spawning puffs,explosions)
-CVAR (co_fixweaponimpacts, "0", "When enabled, activate correct impact of projectiles and bullets on surfaces (spawning puffs,explosions)",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_fixweaponimpacts, "0", "Corrected behavior for impact of projectiles and bullets on surfaces",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Allow line specials with sector tags of 0 to be used for moving floors/ceilings.
-CVAR (co_fixzerotags, "0", "Allow line specials with sector tags of 0 to be used for moving floors/ceilings.",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_fixzerotags, "0", "Allow line specials with sector tag 0 to be used for moving floors/ceilings.",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// [ML] When enabled, additional checks are made on two-sided lines, allows additional
-// silent bfg tricks, and the player will "oof" on two-sided lines
-CVAR (co_boomlinecheck, "0", "additional checks are made on two-sided lines, allows additional silent bfg tricks, and the player will \"oof\" on two-sided lines",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_boomlinecheck, "0", "Make additional checks on two-sided lines, allowing two-sided " \
+					"lines to be used for the silent BFG trick",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Use Boom's algorithm for determining the actors in, or at least touching, a sector
-CVAR (co_boomsectortouch, "0", "Use a finer-grained, faster, and more accurate test for actors that are touching a sector (i.e. those affected if it moves)",
-	  CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_boomsectortouch, "0", "Use a finer-grained, faster, and more accurate test for actors " \
+					"that are touching a sector (i.e. those affected if it moves)",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Enable/disable the "level 8 full sound at far distances" feature
-CVAR (co_level8soundfeature, "0", "Enable/disable the \"level 8 full sound at far distances\" feature",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
+CVAR(				co_level8soundfeature, "0", "Enable/disable the \"level 8 full sound at far distances\" feature",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_LATCH)
 
-// Fix the blockmap collision bug
-CVAR (co_blockmapfix, "0", "Fix the blockmap collision bug",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
+CVAR(				co_blockmapfix, "0", "Fix the blockmap collision bug",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_LATCH)
 
-// Enable/disable zdoom-based gravity and physics interactions
-CVAR (co_zdoomphys, "0", "Enable/disable zdoom-based gravity and physics interactions",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_zdoomphys, "0", "Enable/disable ZDoom-based gravity and physics interactions",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// 0 - Play the switch resetting sound at map location (0,0) like the vanilla bug
-// 1 - switch sounds attenuate with distance like plats and doors.
-CVAR (co_zdoomswitches, "0", "Play switch sounds attenuate with distance",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_zdoomswitches, "0", "Enable attenuation of switch sounds with distance",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Use ZDoom's sound curve instead of vanilla Doom's
-CVAR (co_zdoomsoundcurve, "0", "Use ZDoom's sound curve instead of vanilla Doom's",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_zdoomsoundcurve, "0", "Use ZDoom's sound attenuation curve instead of vanilla Doom's",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Turns off the west-facing silent spawns vanilla bug
-CVAR (co_nosilentspawns, "0", "Turns off the west-facing silent spawns vanilla bug",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_nosilentspawns, "0", "Turns off the west-facing silent spawns vanilla bug",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Increase precision of autoatim
-CVAR (co_fineautoaim, "0", "Increase precision of autoatim",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
+CVAR(				co_fineautoaim, "0", "Increase precision of vertical auto-aim",
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
-// Dead player's view follows the actor who killed them
-CVAR (cl_deathcam, "1", "Dead player's view follows the actor who killed them",
-      CVARTYPE_BOOL, CVAR_ARCHIVE)
+// Client options
+// ---------------------
 
-CVAR (cl_predictsectors, "1", "Move floors and ceilings immediately instead of waiting for confirmation",
-     CVARTYPE_BOOL, CVAR_ARCHIVE)
+CVAR(				cl_deathcam, "1", "Dead player's view follows the actor who killed them",
+					CVARTYPE_BOOL, CVAR_CLIENTARCHIVE)
 
-CVAR (cl_predictpickup, "1", "Predict weapon pickups", CVARTYPE_BOOL, CVAR_ARCHIVE)
+CVAR(				cl_predictsectors, "1", "Move floors and ceilings immediately instead of waiting for confirmation",
+					CVARTYPE_BOOL, CVAR_CLIENTARCHIVE)
 
-// Movebob
-CVAR_FUNC_DECL (cl_movebob, "1.0", "Adjust weapon and movement bobbing",
-      CVARTYPE_BOOL, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE)
+CVAR(				cl_predictpickup, "1", "Predict weapon pickups",
+					CVARTYPE_BOOL, CVAR_CLIENTARCHIVE)
 
-CVAR (cl_rockettrails, "0", "Rocket trails on/off (currently unused)", CVARTYPE_BOOL,  CVAR_ARCHIVE)
+CVAR_RANGE(			cl_movebob, "1.0", "Adjust weapon and movement bobbing",
+					CVARTYPE_FLOAT, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE, 0.0f, 1.0f)
 
-// [AM] Force a player to respawn.
-CVAR (sv_forcerespawn, "0", "Force a player to respawn.",
-      CVARTYPE_BOOL, CVAR_SERVERARCHIVE | CVAR_SERVERINFO)
-// [AM] Force a player to respawn after a set amount of time
-CVAR (sv_forcerespawntime, "30", "Force a player to respawn after a set amount of time",
-      CVARTYPE_WORD, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
-CVAR (sv_spawndelaytime, "0.0", "Force a player to wait a period (in seconds) before respawning",
-      CVARTYPE_FLOAT, CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
+CVAR(				cl_rockettrails, "0", "Rocket trails on/off (currently unused)",
+					CVARTYPE_BOOL, CVAR_CLIENTARCHIVE)
 
-CVAR_FUNC_DECL (sv_gravity, "800", "Gravity of the environment",
-      CVARTYPE_INT, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
-CVAR_FUNC_DECL (sv_aircontrol, "0.00390625", "How much control the player has in the air",
-      CVARTYPE_FLOAT, CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
 
-CVAR_FUNC_DECL (sv_splashfactor, "1.0", "Rocket explosion thrust effect?",
-      CVARTYPE_FLOAT,  CVAR_ARCHIVE | CVAR_SERVERARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE)
+CVAR_RANGE_FUNC_DECL(sv_gravity, "800", "Gravity of the environment",
+					CVARTYPE_WORD, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE,
+					0.0f, 32768.0f)
+
+CVAR_RANGE_FUNC_DECL(sv_aircontrol, "0.00390625", "How much control the player has in the air",
+					CVARTYPE_FLOAT, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE,
+					0.0f, 32768.0f)
+
+CVAR_RANGE_FUNC_DECL(sv_splashfactor, "1.0", "Rocket explosion thrust effect?",
+					CVARTYPE_FLOAT, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_NOENABLEDISABLE,
+					0.01f, 100.0f)
 
 
 // Misc stuff
 // ----------
 
-// debugging mode
-CVAR (developer, "0", "debugging mode",
-      CVARTYPE_BOOL, CVAR_NULL)
+CVAR(				developer, "0", "Debugging mode",
+					CVARTYPE_BOOL, CVAR_NULL)
 
-// Language.
-CVAR_FUNC_DECL (language, "0", "", CVARTYPE_INT, CVAR_ARCHIVE)
+CVAR_RANGE_FUNC_DECL(language, "0", "",
+					CVARTYPE_INT, CVAR_ARCHIVE, 0.0f, 256.0f)
 
-// Port (???)
-CVAR (port, "0", "Display currently used port number",
-      CVARTYPE_INT, CVAR_NOSET | CVAR_NOENABLEDISABLE)
-// Chase camera settings
-CVAR (chase_height,     "-8", "Height of chase camera",
-      CVARTYPE_WORD, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE)
-CVAR (chase_dist,    "90", "Chase camera distance",
-      CVARTYPE_WORD, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE)
-// Generate centerview when -mlook encountered?
-CVAR (lookspring,    "1", "Generate centerview when mlook encountered",
-      CVARTYPE_BOOL, CVAR_CLIENTARCHIVE)
-// Allows players to walk through other players
-CVAR (sv_unblockplayers, "0", "Allows players to walk through other players",
-      CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_LATCH | CVAR_SERVERINFO)
-// [Spleen] Allow custom WAD directories to be specified in a cvar
-CVAR (waddirs, "", "Allow custom WAD directories to be specified",
-      CVARTYPE_STRING, CVAR_ARCHIVE | CVAR_NOENABLEDISABLE)
-// [Xyltol 02/27/2012] Hostname retrieval for Scoreboard
-CVAR (sv_hostname,      "Untitled Odamex Server", "Server name to appear on masters, clients and launchers",
-   CVARTYPE_STRING, CVAR_SERVERARCHIVE | CVAR_NOENABLEDISABLE | CVAR_SERVERINFO)
+CVAR(				port, "0", "Display currently used network port number",
+					CVARTYPE_INT, CVAR_NOSET | CVAR_NOENABLEDISABLE)
+
+CVAR_RANGE(			chase_height, "-8", "Height of chase camera",
+					CVARTYPE_WORD, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE, -32768.0f, 32768.0f)
+
+CVAR_RANGE(			chase_dist, "90", "Chase camera distance",
+					CVARTYPE_WORD, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE, -32768.0f, 32768.0f)
+
+CVAR(				lookspring, "1", "Generate centerview when mlook encountered",
+					CVARTYPE_BOOL, CVAR_CLIENTARCHIVE)
+
+CVAR(				waddirs, "", "Allow custom WAD directories to be specified",
+					CVARTYPE_STRING, CVAR_ARCHIVE | CVAR_NOENABLEDISABLE)
 
 // Experimental settings (all categories)
 // =======================================
 
-// Speedhack code (server related)
-CVAR (sv_speedhackfix,     "0", "Experimental anti-speedhack code",
-      CVARTYPE_BOOL, CVAR_SERVERARCHIVE)
+CVAR(				sv_speedhackfix, "0", "Experimental anti-speedhack code",
+					CVARTYPE_BOOL, CVAR_SERVERARCHIVE)
 
 VERSION_CONTROL (c_cvarlist_cpp, "$Id$")

common/c_cvars.cpp
@@ -555,8 +555,8 @@ void cvar_t::C_ArchiveCVars (void *f)
 
 	while (cvar)
 	{
-		if ((cvar->m_Flags & CVAR_ARCHIVE) || (baseapp == client && cvar->m_Flags & CVAR_CLIENTARCHIVE)
-			|| (baseapp == server && cvar->m_Flags & CVAR_SERVERARCHIVE))
+		if ((baseapp == client && (cvar->m_Flags & CVAR_CLIENTARCHIVE))
+			|| (baseapp == server && (cvar->m_Flags & CVAR_SERVERARCHIVE)))
 		{
 			fprintf ((FILE *)f, "// %s\n", cvar->helptext());
 			fprintf ((FILE *)f, "set %s %s\n\n", C_QuoteString(cvar->name()).c_str(), C_QuoteString(cvar->cstring()).c_str());

common/c_cvars.h
@@ -40,7 +40,6 @@ CVARS (console variables)
 */
 
 #define CVAR_NULL               0 // [deathz0r] no special properties
-#define CVAR_ARCHIVE            1 // set to cause it to be saved to vars.rc
 #define CVAR_USERINFO           2 // added to userinfo  when changed
 #define CVAR_SERVERINFO         4 // [Toke - todo] Changed the meaning of this flag
                                   // it now describes cvars that clients will be
@@ -56,6 +55,9 @@ CVARS (console variables)
 #define CVAR_NOENABLEDISABLE 1024 // [Nes] No substitution (0=disable, 1=enable)
 #define CVAR_SERVERARCHIVE   4096 // [Nes] Server version of CVAR_ARCHIVE
 #define CVAR_CLIENTARCHIVE   8192 // [Nes] Client version of CVAR_ARCHIVE
+
+// [SL] CVAR_ARCHIVE enables both CVAR_CLIENTARCHIVE & CVAR_SERVERARCHIVE 
+#define CVAR_ARCHIVE	(CVAR_CLIENTARCHIVE | CVAR_SERVERARCHIVE)
 
 // Hints for network code optimization
 typedef enum

common/p_map.cpp
@@ -2720,12 +2720,9 @@ static int			bombmod;
 // [RH] Damage scale to apply to thing that shot the missile. (co_zdoomphys)
 static float selfthrustscale;
 
-CVAR_FUNC_IMPL (sv_splashfactor)
+CVAR_FUNC_IMPL(sv_splashfactor)
 {
-	if (var <= 0.0f)
-		var.Set (1.0f);
-	else
-		selfthrustscale = 1.0f / var;
+	selfthrustscale = 1.0f / var;
 }
 
 

common/p_pspr.cpp
@@ -54,14 +54,7 @@ EXTERN_CVAR(sv_freelook)
 EXTERN_CVAR(sv_allowmovebob)
 EXTERN_CVAR(sv_allowpwo)
 EXTERN_CVAR(co_fineautoaim)
-
-CVAR_FUNC_IMPL(cl_movebob)
-{
-	if (var > 1.0f)
-		var.Set(1.0f);
-	if (var < 0.0f)
-		var.Set(0.0f);
-}
+EXTERN_CVAR(cl_movebob)
 
 const char *weaponnames[] =
 {

common/p_setup.cpp
@@ -1736,29 +1736,13 @@ void P_Init (void)
 
 // [ML] Do stuff when the timelimit is reset
 // Where else can I put this??
-CVAR_FUNC_IMPL (sv_timelimit)
+CVAR_FUNC_IMPL(sv_timelimit)
 {
-	if (var < 0)
-		var.Set(0.0f);
-
-	// timeleft is transmitted as a short so cap the sv_timelimit at the maximum
-	// for timeleft, which is 9.1 hours
-	if (var > MAXSHORT / 60)
-		var.Set(MAXSHORT / 60);
-
 	level.timeleft = var * TICRATE * 60;
 }
 
-CVAR_FUNC_IMPL (sv_intermissionlimit)
+CVAR_FUNC_IMPL(sv_intermissionlimit)
 {
-	if (var < 0)
-		var.Set(0.0f);
-
-	// intermissionleft is transmitted as a short so cap the sv_timelimit at the maximum
-	// for timeleft, which is 9.1 hours
-	if (var > MAXSHORT)
-		var.Set(MAXSHORT);
-
 	level.inttimeleft = (var < 1 ? DEFINTSECS : var);
 }
 

commit 2c7c4d2d5a5f5039c773b89f19294796f4e377f8

-Fix a demo desync bug introduced in r4521 that caused co_blockmapfix to not be disabled when playing vanilla demos.
git-svn-id: http://odamex.net/svn/root/trunk@4549 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/c_cvarlist.cpp
@@ -192,10 +192,10 @@ CVAR(				co_boomsectortouch, "0", "Use a finer-grained, faster, and more accurat
 					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)
 
 CVAR(				co_level8soundfeature, "0", "Enable/disable the \"level 8 full sound at far distances\" feature",
-					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_LATCH)
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
 CVAR(				co_blockmapfix, "0", "Fix the blockmap collision bug",
-					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_LATCH)
+					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)
 
 CVAR(				co_zdoomphys, "0", "Enable/disable ZDoom-based gravity and physics interactions",
 					CVARTYPE_BOOL, CVAR_ARCHIVE | CVAR_SERVERINFO)